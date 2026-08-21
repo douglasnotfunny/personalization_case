ca0d1899d2aaf1c7806e091caa00f0dbcbf26c51
douglasnotfunny <90223473+douglasnotfunny@users.noreply.github.com>
2026-08-20 22:05:19 -0300

    chore: finalize personalization case

--- app/feature_processing.py
+++ app/feature_processing.py
@@ -1,70 +1,47 @@
 import pandas as pd
 
-
 def build_feature_dataset(
     events: pd.DataFrame,
     products: pd.DataFrame,
 ) -> pd.DataFrame:
-    
-    interactions = (
-        events
-        .groupby(["user_id", "product_id"])
-        .size()
-        .reset_index(name="interactions")
+
+    events = events[['user_id', 'product_id']].copy()
+
+    event_product = pd.merge(events, products, on='product_id', 
+                             how='left', validate='many_to_one')
+
+    event_product['interactions'] = (event_product.groupby(['user_id', 'product_id'])
+                                     ['user_id'].transform('count')
+    )
+    event_product['interactions_by_category'] = (event_product.groupby(['user_id', 'category'])
+                                         ['user_id'].transform('count')
     )
 
-    events_with_products = events.merge(
-        products[["product_id", "category"]],
-        on="product_id",
-        how="left",
+    max_affinity = (
+        event_product.sort_values(['user_id', 'interactions_by_category'], ascending=[True, False])
+        .drop_duplicates('user_id')[['user_id', 'category']]
+        .rename(columns={'category': 'user_affinity_category'})
     )
 
-    user_category_counts = (
-        events_with_products
-        .groupby(["user_id", "category"])
-        .size()
-        .reset_index(name="category_interactions")
+    users = events[['user_id']].drop_duplicates()
+    feature_df = pd.merge(users, products, how='cross')
+
+    interactions = (event_product[['user_id', 'product_id', 'interactions']]
+                    .drop_duplicates(['user_id', 'product_id'])
     )
+    feature_df = pd.merge(feature_df, interactions, on=['user_id', 'product_id'], 
+                      how='left', validate='one_to_one')
 
-    user_affinity = (
-        user_category_counts
-        .sort_values(
-            ["user_id", "category_interactions"],
-            ascending=[True, False],
-        )
-        .drop_duplicates("user_id")
-        [["user_id", "category"]]
-        .rename(columns={"category": "user_affinity_category"})
-    )
+    feature_df['interactions'] = feature_df['interactions'].fillna(0).astype(int)
 
-    users = events[["user_id"]].drop_duplicates()
+    feature_df = pd.merge(feature_df, max_affinity, on='user_id', 
+                          how='left', validate='many_to_one')
 
-    user_products = users.merge(
-        products,
-        how="cross",
-    )
-
-    feature_df = user_products.merge(
-        interactions,
-        on=["user_id", "product_id"],
-        how="left",
-    )
-
-    feature_df["interactions"] = (
-        feature_df["interactions"]
-        .fillna(0)
-        .astype(int)
-    )
-
-    feature_df = feature_df.merge(
-        user_affinity,
-        on="user_id",
-        how="left",
-    )
-
-    feature_df["user_affinity_match"] = (
-        feature_df["category"]
-        == feature_df["user_affinity_category"]
+    feature_df['user_affinity_match'] = (
+        feature_df['category'] == feature_df['user_affinity_category']
     ).astype(int)
+
+    feature_df = feature_df.drop(columns=['user_affinity_category'])
+    feature_df.to_csv("data/feature_dataset.csv", index=False)
 
     return feature_df
